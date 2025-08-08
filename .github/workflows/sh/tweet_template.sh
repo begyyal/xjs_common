976d57d41a3ffe7eab162ab36c62eb4ec801b5c5
begyyal <begyyal@gmail.com>
2025-08-09 04:51:35 +0900

    change trigger of publishing workflow to dispatch.

--- .github/workflows/sh/tweet_template.sh
+++ .github/workflows/sh/tweet_template.sh
@@ -2,9 +2,9 @@
 
 tag_name=$1
 repo_url=$2
-repos_name=${repo_url##*/}
+name="xjs-common"
 ext="#javascript #typescript #utility #npm"
 LF=$'\\n'
-text="${repos_name}@${tag_name} was published.${LF}${repo_url}"
+text="${name}@${tag_name} was published.${LF}${repo_url}"
 [ -n "$ext" ] && text=${text}${LF}${ext} || :
 echo -n "{\"text\":\"${text}\"}"
---
+++ publish.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ghtoken=$(cat ./.ghtoken)
+curl -L \
+  -X POST \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $ghtoken" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/begyyal/xjs_common/actions/workflows/publish.yml/dispatches \
+  -d '{"ref":"main"}'
