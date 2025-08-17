ed73f121f98b7fc6502bdc916b2410c4a3ac2f7e
begyyal <begyyal@gmail.com>
2025-08-17 09:50:26 +0900

    add primitive negate types.

--- src/const/types.ts
+++ src/const/types.ts
@@ -5,6 +5,7 @@ export type MaybeArray<T> = T | T[];
 export type Loggable = { log: (msg: any) => void, warn: (msg: any) => void, error: (msg: any) => void };
 export type Unique<T = number> = { id: T };
 export type IdName<T = number> = { name: string } & Unique<T>;
+export type Any = string | number | bigint | boolean | symbol | object | undefined | null;
 export enum Type {
     string = "string",
     number = "number",
@@ -15,3 +16,11 @@ export enum Type {
     undefined = "undefined",
     null = "null"
 }
+// temporary types until typescript implements negate type.
+// https://github.com/microsoft/TypeScript/issues/4196
+export type NonString = Exclude<Any, string>;
+export type NonNumber = Exclude<Any, number>;
+export type NonBigint = Exclude<Any, bigint>;
+export type NonBoolean = Exclude<Any, boolean>;
+export type NonSymbol = Exclude<Any, symbol>;
+export type NonObject = Exclude<Any, object>;
