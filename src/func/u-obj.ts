2ed24efec596024e81c194e8c81ed904367b02d7
begyyal <begyyal@gmail.com>
2025-08-17 12:09:39 +0900

    add manipulateProperties() to UObj.

--- src/func/u-obj.ts
+++ src/func/u-obj.ts
@@ -1,4 +1,4 @@
-import { NormalRecord } from "../const/types";
+import { MaybeArray, NonObject, NormalRecord, Type } from "../const/types";
 import { DType, smbl_tm, TypeMap } from "./decorator/d-type";
 import { UType } from "./u-type";
 
@@ -20,7 +20,7 @@ export namespace UObj {
     }
     /**
      * crop properties of the object. the properties is removed with `delete` operator.
-     * @param o object which properties is removed.
+     * @param o object whose properties is removed.
      * @param keys property names to be remained. if omit this, it removes the properties other than properties decorated {@link DType}.
      * @param exclusive if true, it removes `keys` instead of remaining it.
      */
@@ -31,6 +31,32 @@ export namespace UObj {
             if (!keys && (o[smbl_tm] as TypeMap)?.[k]?.rec && o[k]) crop(o[k]);
             return !!exclusive === _keys.includes(k);
         }).forEach(k => delete o[k]);
+        return o;
+    }
+    /**
+     * manipulate properties of an object. 
+     * as default if the properties contains object, it also manipulates properties of that recursively.
+     * @param o object whose properties the process applies to.
+     * @param process process to be applied to properties of the object. note that function property is not included in the properties.
+     * @param op.recursive whether it manipulate properties of an object recursively. default is true.
+     * @param op.targetType primitive types which filter the properties to be processed.
+     */
+    export function manipulateProperties<T extends NormalRecord>(
+        o: T, process: (v: NonObject) => NonObject, op?: {
+            recursive?: boolean,
+            targetType?: MaybeArray<Exclude<Type, "object" | "null" | "undefined">>,
+        }): T {
+        const target = op?.targetType && UType.takeAsArray(op?.targetType);
+        const _recursive = !UType.isDefined(op?.recursive) || op?.recursive;
+        const rec = (_o: object) => {
+            for (const k in _o) {
+                const prop = _o[k];
+                if (UType.isObject(prop) && _recursive) rec(prop);
+                else if (!UType.isFunction(prop) && (!target || target.some(t => typeof prop === t)))
+                    _o[k] = process(prop);
+            }
+        };
+        rec(o);
         return o;
     }
 }
