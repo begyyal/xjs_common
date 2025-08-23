baf54a9acc718b84b4ee1659a483871d738e4ee0
begyyal <begyyal@gmail.com>
2025-08-23 13:34:17 +0900

    refactoring.

--- src/func/u-array.ts
+++ src/func/u-array.ts
@@ -5,11 +5,11 @@ export namespace UArray {
     /** 
      * compares two arrays to valuate equality. 
      * if one side is null or undefined, it returns true when other side is the same.
-     * this function has compatibility to `AlmostArray` like `Uint8Array` etc.
+     * this function has compatibility {@link AlmostArray} like {@link Uint8Array} etc.
      * @param v1 it uses equal operator for comparing elements, so applying object element is not recommended.
      * @param v2 same as v1.
      * @param sort it uses {@link Array.sort()} on v1 and v2 if true. default is true.
-     * @param useStrictEqual check equality whether `Array` or not and it uses `===` operator for compareing elements if true, otherwise using `==` operator. default is true.
+     * @param useStrictEqual check equality whether {@link Array} or not and it uses `===` operator for compareing elements if true, otherwise using `==` operator. default is true.
      */
     export function eq(v1: any[] | AlmostArray, v2: any[] | AlmostArray, op: { sort?: boolean, useStrictEqual?: boolean } = {}): boolean {
         const { sort, useStrictEqual } = Object.assign({ sort: true, useStrictEqual: true }, op);
@@ -41,11 +41,11 @@ export namespace UArray {
     }
     /**
      * chop array to partial arrays which have specified length. the remainder is added to end of a result.  
-     * this function has compatibility to `AlmostArray` like `Uint8Array` etc.
+     * this function has compatibility to {@link AlmostArray} like {@link Uint8Array} etc.
      */
     export function chop<E>(array: E[], len: number): E[][];
     export function chop<T extends AlmostArray>(array: T, len: number): T[];
-    export function chop<E>(array: E[] | AlmostArray<E>, len: number): (E[] | AlmostArray<E>)[] {
+    export function chop<T extends AlmostArray>(array: T, len: number): T[] {
         return [...Array(Math.ceil(array.length / len)).keys()]
             .map(i => {
                 let endIdx = (i + 1) * len;
