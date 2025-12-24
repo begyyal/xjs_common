b7b2395d5052aefc849c5cb7a54ab2d65a390bb8
begyyal <begyyal@gmail.com>
2025-12-24 20:59:46 +0900

    fix up lack of export for Array2.

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ export * from "./func/u-string";
 export * from "./func/u-array";
 export * from "./func/u-http";
 export * from "./func/u-type";
+export * from "./func/array2";
 export * from "./func/decorator/transaction";
 export { DType } from "./func/decorator/d-type";
 
